7f35c5e303711556ee00edb558c7eaf7528c1cbb
AnjaliS4 <anjalisimkhada5@gmail.com>
2025-12-03 08:36:34 +0545

    made changes in some strings

--- Week6assign/index.py
+++ Week6assign/index.py
@@ -2,26 +2,26 @@
 import numpy as np
 import pandas as pd
 
-# ------------------------------
-# Loading your dataset
-# ------------------------------
+# Anjaliii 
+# Loading our dataset
+
 df = pd.read_csv("Myers Briggs Table_S1.csv")
 
-# ------------------------------
+
 # Preparing the sample data (first 5 rows)
 # Creating unique names as MBTI_SNO
-# ------------------------------
+
 sample = df.head(5).copy()
 sample["name"] = sample["MBTI"] + "_" + sample["S No"].astype(str)
 
-# Extracting required fields
+# Extracting required fields from our dataset 
 names = sample["name"].values
 ages = sample["AGE"].values
 heights = sample["HEIGHT"].values
 
-# ------------------------------
+
 # TASK A: Create structured array
-# ------------------------------
+# Anjaliii 
 
 data_type = [
     ("name", "U20"),     # string
@@ -39,28 +39,28 @@ print("TASK A: Structured Array")
 print(structured_array)
 print("\n")
 
-# ------------------------------
+
 # TASK B: Print all values of the 'name' field
-# ------------------------------
+# Anjalii 
+
 print("TASK B: All Names")
 print(structured_array["name"])
 print("\n")
 
-# ------------------------------
+
 # TASK C: Update the age of the first individual to 30
-# ------------------------------
+# Anjaliii 
 structured_array["age"][0] = 30
 
 print("TASK C: After Updating First Person's Age to 30")
 print(structured_array)
 print("\n")
 
-# ------------------------------
-# TASK D: Add a new record (Nadine Smith, 25, 5.9)
-# ------------------------------
-new_record = np.array([("Nadine Smith", 25, 5.9)], dtype=data_type)
+# TASK D: Add a new record (Anjali Simkhada, 20, 5.1)
+
+new_record = np.array([("Anjali Simkhada", 20, 5.1)], dtype=data_type)
 
 updated_array = np.concatenate((structured_array, new_record))
 
-print("TASK D: After Adding Nadine Smith")
+print("TASK D: After Adding Anjali Simkhada")
 print(updated_array)
